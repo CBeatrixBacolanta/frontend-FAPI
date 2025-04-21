26b57a3cbaaef59247db108b66be67ab9629cedf
CBeatrixBacolanta <bacolanta.celeste173@gmail.com>
2025-04-21 22:45:02 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace with your repo name
 export default defineConfig({
   base: '/frontend-FAPI/',
   plugins: [react()],
