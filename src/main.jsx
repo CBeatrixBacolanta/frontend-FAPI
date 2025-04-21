70fac5daeb849a923007d915d2505752feac3af7
CBeatrixBacolanta <bacolanta.celeste173@gmail.com>
2025-04-21 22:02:53 +0800

    6th commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
-const root = document.getElementById("root"); 
-
-if (root) {
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/CBeatrixBacolanta">
       <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error("❌ Root element not found! Check index.html");
-}
+    </BrowserRouter>
+  </React.StrictMode>
+)
